a070195ecc5e700a9e226f81da48e0b646c86ccb
taejun-hub <xowns1016@daou.co.kr>
2025-01-15 10:23:45 +0900

    Create [실습][SQL]250115-실습과제4.sql

---
+++ [실습][SQL]250115-실습과제4.sql
@@ -0,0 +1,15 @@
+-- 고객 테이블에서 모든 고객의 이름과 이메일을 출력
+SELECT NAME FROM CUSTOMERS
+ORDER BY NAME ASC;
+
+-- 계좌 테이블에서 잔액이 높은 순으로 계좌 ID와 잔액을 출력
+SELECT ACCOUNT_ID, BALANCE FROM ACCOUNTS
+ORDER BY BALANCE DESC;
+
+-- 거래 테이블에서 거래 금액이 낮은 순으로 거래 ID와 금액을 출력
+SELECT TRANSACTION_ID, AMOUNT FROM TRANSACTIONS
+ORDER BY AMOUNT;
+
+-- 대출 테이블에서 대출 금액이 높은 순으로 대출 ID와 고객 ID를 출력
+SELECT LOAN_ID, CUSTOMER_ID FROM LOANS
+ORDER BY AMOUNT DESC;
