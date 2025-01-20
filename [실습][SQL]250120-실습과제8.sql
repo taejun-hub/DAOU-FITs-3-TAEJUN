f272960dbf64a5cf3a96fec08655dcd703f8eab5
taejun-hub <xowns1016@daou.co.kr>
2025-01-20 11:25:32 +0900

    Create [실습][SQL]250120-실습과제8.sql

---
+++ [실습][SQL]250120-실습과제8.sql
@@ -0,0 +1,32 @@
+-- 실습 8.
+
+--1. 지점 이름이 'Location 1'에 해당하는 지점에서 대출을 받은 고객의 이름을 출력
+SELECT NAME
+FROM CUSTOMERS
+WHERE CUSTOMER_ID IN (
+    SELECT CUSTOMER_ID
+    FROM LOANS
+    WHERE BRANCH_ID IN (
+        SELECT BRANCH_ID
+        FROM BRANCHES
+        WHERE LOCATION = 'Location 1'
+    )
+);
+
+--2. 특정 고객(CUSTOMER_ID = 100)이 소유한 모든 계좌에서 발생한 거래를 출력
+SELECT * 
+FROM TRANSACTIONS
+WHERE ACCOUNT_ID IN (
+    SELECT ACCOUNT_ID
+    FROM ACCOUNTS
+    WHERE CUSTOMER_ID = 100
+);
+
+--3. 각 고객이 보유한 계좌 중 가장 높은 잔액을 가진 계좌를 조회
+SELECT ACCOUNT_ID, CUSTOMER_ID, BALANCE
+FROM ACCOUNTS A1
+WHERE BALANCE = (
+    SELECT MAX(BALANCE) 
+    FROM ACCOUNTS A2
+    WHERE A1.CUSTOMER_ID = A2.CUSTOMER_ID
+);
